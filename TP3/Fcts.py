181d6965ec8eba8120b3368683ff3efb9491c4bb
Florian Trémouille <florian.termouille@cpe.fr>
2022-11-29 08:41:20 +0100

    1

---
+++ TP3/Fcts.py
@@ -0,0 +1,8 @@
+'''
+Fonctions du programme du pendu TP3
+29/11/2022
+Author : Hugo MIAGLIA & Florian TREMOUILLE
+'''
+def FindWord(Fichier):
+    Mots = open(Fichier,'r')
+    print(Mots)
---
+++ TP3/Pendu.py
@@ -0,0 +1,10 @@
+'''
+Programme du pendu TP3
+29/11/2022
+Author : Hugo MIAGLIA & Florian TREMOUILLE
+'''
+
+from Fcts import FindWord
+
+fich=open('mots.txt','r')
+print(fich)
