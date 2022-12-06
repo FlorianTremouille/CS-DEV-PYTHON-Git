22037b999e85c26349e3b4caf963ac51027d0830
Hugo MIAGLIA <hugo.miaglia@cpe.fr>
2022-12-06 11:23:35 +0100

    début projectile

---
+++ TP456-Space_Invader/entities/projectile.py
@@ -0,0 +1,19 @@
+class Projectile :
+
+    def __init__(self,x,y,w,h):
+        self.x=x
+        self.y=y
+        self.w=w
+        self.h=h
+
+    def projectile_Init(self,canvas):
+        self.projectile = canvas.create_rectangle(
+            self.x,
+            self.y,
+            self.x + self.w,
+            self.y + self.h,
+            fill = 'red'           
+        )
+        return self.projectile
+
+    
