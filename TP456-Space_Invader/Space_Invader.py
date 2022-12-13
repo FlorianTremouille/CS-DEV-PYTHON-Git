020b2a5c963ff8e6d05e5619bf24fd9f8667fb76
Florian Trémouille <florian.termouille@cpe.fr>
2022-12-13 10:40:16 +0100

    10h40

--- TP456-Space_Invader/Space_Invader.py
+++ TP456-Space_Invader/Space_Invader.py
@@ -1,6 +1,9 @@
 import tkinter as Tk
 from entities.World import World
 
+VITESSE_ENNEMI_X = 5
+VITESSE_ENNEMI_Y = 50
+
 Window = Tk.Tk()
 Window.title('Space INVADATEUR')
 Window.geometry('1000x800')
@@ -18,7 +21,7 @@ ENNEMIS_OBJECT = map.W_Ennemis
 # Ennemi = canvas.create_rectangle(0, 0, 50, 50, fill='red')
 
 def EnnemiMove(way):
-    x = 5
+    x = VITESSE_ENNEMI_X
     y = 0
     CoordsLast = canvas.coords(ENNEMI_TAG_LST[-1])
     CoordsFirst = canvas.coords(ENNEMI_TAG_LST[0])
@@ -26,7 +29,7 @@ def EnnemiMove(way):
     if CoordsLast[2] > 750 and way == 1 :
         way = -1
     elif CoordsFirst[0] < 50 and way == -1 :
-        y = 50
+        y = VITESSE_ENNEMI_Y
         way = 1
     
     for ennemi in ENNEMI_TAG_LST:
@@ -34,20 +37,22 @@ def EnnemiMove(way):
             canvas.move(ennemi,x,y)
         elif way == -1:
             canvas.move(ennemi,-x,y)
-    Window.after(42,lambda: EnnemiMove(way))
+    Window.after(40,lambda: EnnemiMove(way))
 
 
 def left(e):
-   x = -20
-   y = 0
-   canvas.move(PLAYER_CANVAS, x, y)
-   map.W_Player.set(canvas.coords(PLAYER_CANVAS)[2],canvas.coords(PLAYER_CANVAS)[1])
+    if canvas.coords(PLAYER_CANVAS)[0] > 25:
+        x = -20
+        y = 0
+        canvas.move(PLAYER_CANVAS, x, y)
+        map.W_Player.set(canvas.coords(PLAYER_CANVAS)[2],canvas.coords(PLAYER_CANVAS)[1])
 
 def right(e):
-   x = 20
-   y = 0
-   canvas.move(PLAYER_CANVAS, x, y)
-   map.W_Player.set(canvas.coords(PLAYER_CANVAS)[2],canvas.coords(PLAYER_CANVAS)[1])
+    if canvas.coords(PLAYER_CANVAS)[2] < 775:
+        x = 20
+        y = 0
+        canvas.move(PLAYER_CANVAS, x, y)
+        map.W_Player.set(canvas.coords(PLAYER_CANVAS)[2],canvas.coords(PLAYER_CANVAS)[1])
 
 
 def ProjMove(proj):  # fonction qui fait bouger les projectiles
@@ -65,14 +70,13 @@ def Tir(e) :
     PlayerProj = map.W_Player.PlayerProj_Init(canvas,x,y)
     ProjMove(PlayerProj)
 
-
 # Bind the move function
 Window.bind("<Left>", left)
 Window.bind("<Right>", right)
 Window.bind("<space>", Tir)
 
-# Initialisation
 
+# Initialisation
 Window.after(100,lambda: EnnemiMove(1))
 Window.after(100,ProjMove)
 
