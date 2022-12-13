0b0fe9b9bf849ab81de56111e39817b1ee14298e
Florian Trémouille <florian.termouille@cpe.fr>
2022-12-13 09:16:14 +0100

    9h16

--- TP456-Space_Invader/Space_Invader.py
+++ TP456-Space_Invader/Space_Invader.py
@@ -8,47 +8,48 @@ Window.geometry('1000x800')
 canvas = Tk.Canvas(Window, width='800', height='700', bg = 'gray')
 canvas.pack(side= "left", padx= 5, pady= 5)
 
-# Vaisseau = Canvas.create_rectangle(0, 648, 50, 698, fill='blue')
-
 map = World(canvas)
-PLAYER = map.W_Player.Can_Player
 
-dENNEMI = map.W_Ennemis
-# Ennemi = canvas.create_rectangle(0, 0, 50, 50, fill='red')
+PLAYER_CANVAS = map.W_Player.Can_Player
+ENNEMI_TAG_LST = map.W_Ennemis.get()
+ENNEMIS_OBJECT = map.W_Ennemis
 
 def EnnemiMove(way):
-    x = 50
-    CoordsLast = canvas.coords(dENNEMI.values()[-1])
-    CoordsFirst = canvas.coords(dENNEMI.values()[0])
+    x = 5
+    y = 0
+    CoordsLast = canvas.coords(ENNEMI_TAG_LST[-1])
+    CoordsFirst = canvas.coords(ENNEMI_TAG_LST[0])
     
     if CoordsLast[2] > 750 and way == 1 :
         way = -1
     elif CoordsFirst[0] < 50 and way == -1 :
+        y = 50
         way = 1
-
-    if way == 1:
-        canvas.move(Ennemi,x,0)
-        Window.after(500,lambda: EnnemiMove(way))
-    elif way == -1:
-        canvas.move(Ennemi,-x,0)
-        Window.after(500,lambda: EnnemiMove(way))
+    
+    for ennemi in ENNEMI_TAG_LST:
+        if way == 1:
+            canvas.move(ennemi,x,y)
+        elif way == -1:
+            canvas.move(ennemi,-x,y)
+    Window.after(42,lambda: EnnemiMove(way))
 
 
 def left(e):
    x = -20
    y = 0
-   canvas.move(PLAYER, x, y)
+   canvas.move(PLAYER_CANVAS, x, y)
 
 def right(e):
    x = 20
    y = 0
-   canvas.move(PLAYER, x, y)
+   canvas.move(PLAYER_CANVAS, x, y)
 
 # Bind the move function
 Window.bind("<Left>", left)
 Window.bind("<Right>", right)
 
 # Initialisation
-Window.after(100,lambda: EnnemiMove(1))
+# Window.after(1000,ENNEMIS_OBJECT.EnnemiMove(canvas,1))
+EnnemiMove(1)
 
 Window.mainloop()
