f5dd0933e9fd3ebcbaba7831563bf28ac135f592
FlorianTremouille <florian.tremouille1@gmail.com>
2022-12-22 19:59:19 +0100

    Minor changes

--- TP456-Space_Invader/entities/Enemy.py
+++ TP456-Space_Invader/entities/Enemy.py
@@ -11,21 +11,24 @@ class Enemy:
 
     def get_canvas(self):
         return self.__canvas
+
     def get_id(self):
         return self.__id
+
     def get_width(self):
         return self.__width
+
     def get_height(self):
         return self.__height
+
     def get_color(self):
         return self.__color
-        
+
     def get_interval_id(self):
         return self.__interval_id
 
     def set_id(self, id):
         self.__id = id
-
 
     def check_for_collision(self):
         c = self.__canvas.coords(self.__id)
@@ -35,7 +38,7 @@ class Enemy:
                 self.die()
 
         self.__interval_id = self.__canvas.after(30, lambda: self.check_for_collision())
-    
+
     def die(self):
         self.__canvas.after_cancel(self.__interval_id)
         self.__canvas.delete(self.__id)
