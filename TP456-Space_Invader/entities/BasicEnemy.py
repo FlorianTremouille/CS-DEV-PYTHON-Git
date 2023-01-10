50383865b73a1340ea68167a3fde611224b7ed09
Hugo MIAGLIA <hugo.miaglia@cpe.fr>
2023-01-10 09:55:19 +0100

    comentaire polygone

--- TP456-Space_Invader/entities/BasicEnemy.py
+++ TP456-Space_Invader/entities/BasicEnemy.py
@@ -45,7 +45,7 @@ class BasicEnemy:
     def check_for_collision(self):        
         continue_check = False
         c = self.get_canvas().coords(self.get_id())
-        entitites = self.get_canvas().find_overlapping(c[4], c[7], c[20], c[1]) # a changer a cause du polygone       
+        entitites = self.get_canvas().find_overlapping(c[4], c[7], c[20], c[1]) # adapté au polygone     
         
         for widget in entitites:
             for tag in self.get_canvas().gettags(widget):
